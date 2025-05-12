bbfddf1d962df7000a24d2928142750b6bb5c013
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-12 14:18:34 +0200

    not mach

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,180 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+      home: const ProfilePage(),
     );
   }
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String name = "Hasan Abouminyar";
+  String email = "h.abouminyar@example.com";
+  String phone = "+218 92 000 0000";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+        actions: [
+          // زر التعديل في شريط العنوان
+          IconButton(
+            icon: const Icon(Icons.edit), // أيقونة القلم
+            onPressed: () async {
+              // عند الضغط على زر التعديل، ننتقل إلى صفحة التعديل
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder:
+                      (context) => EditProfilePage(
+                        name: name, // نرسل الاسم الحالي
+                        email: email, // نرسل البريد الإلكتروني الحالي
+                        phone: phone, // نرسل رقم الهاتف الحالي
+                      ),
+                ),
+              );
+
+              // بعد العودة من صفحة التعديل، نتحقق إذا كانت هناك بيانات جديدة
+              if (result != null) {
+                setState(() {
+                  // نحدث البيانات باستخدام القيم الجديدة
+                  name = result['name'];
+                  email = result['email'];
+                  phone = result['phone'];
+                });
+              }
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/Profile.jpg'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              name,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+
+            Text(
+              email,
+              style: const TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 10),
+
+            Text(
+              phone,
+              style: const TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditProfilePage extends StatefulWidget {
+  final String name;
+  final String email;
+  final String phone;
+
+  const EditProfilePage({
+    super.key,
+    required this.name,
+    required this.email,
+    required this.phone,
+  });
+
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  late TextEditingController nameController;
+  late TextEditingController emailController;
+  late TextEditingController phoneController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    nameController = TextEditingController(text: widget.name);
+    emailController = TextEditingController(text: widget.email);
+    phoneController = TextEditingController(text: widget.phone);
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    phoneController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Profile"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column( 
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: phoneController,
+              decoration: const InputDecoration(
+                labelText: "Phone",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // عند الضغط على زر الحفظ، نعيد البيانات المعدلة
+                Navigator.pop(context, {
+                  'name': nameController.text,
+                  'email': emailController.text,
+                  'phone': phoneController.text,
+                });
+              },
+              child: const Text("Save"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
 }
