8a330a6048b2ca3247bd5789a93ef5ef4202efb7
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-25 02:14:23 +0200

    add new work

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class AppleStoreCloneApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
         scaffoldBackgroundColor: Colors.white,
-        textTheme: GoogleFonts.sfProTextTheme(), // خط يشبه Apple
+        textTheme: GoogleFonts.robotoTextTheme(), // استخدام خط Roboto كبديل
         appBarTheme: AppBarTheme(
           backgroundColor: Colors.white,
           elevation: 0,
@@ -43,17 +43,20 @@ class StorePage extends StatelessWidget {
   final List<Product> products = [
     Product(
       name: 'iPhone 15 Pro',
-      imageUrl: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-blue-titanium',
+      imageUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-blue-titanium',
       price: 'From \$999',
     ),
     Product(
       name: 'MacBook Air M2',
-      imageUrl: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_EMEA_LANG_EN',
+      imageUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_EMEA_LANG_EN',
       price: 'From \$1099',
     ),
     Product(
       name: 'Apple Watch Series 9',
-      imageUrl: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-s9-select-202309_GEO_EMEA_LANG_EN',
+      imageUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-s9-select-202309_GEO_EMEA_LANG_EN',
       price: 'From \$399',
     ),
   ];
@@ -61,10 +64,7 @@ class StorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Store'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: Text('Store'), centerTitle: true),
       body: ListView.builder(
         padding: const EdgeInsets.all(16),
         itemCount: products.length,
@@ -85,13 +85,16 @@ class StorePage extends StatelessWidget {
                   width: 60,
                   height: 60,
                   fit: BoxFit.cover,
-                  placeholder: (context, url) =>
-                      CircularProgressIndicator(strokeWidth: 2),
+                  placeholder:
+                      (context, url) =>
+                          CircularProgressIndicator(strokeWidth: 2),
                   errorWidget: (context, url, error) => Icon(Icons.error),
                 ),
               ),
-              title: Text(product.name,
-                  style: TextStyle(fontWeight: FontWeight.bold)),
+              title: Text(
+                product.name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
               subtitle: Text(product.price),
               trailing: Icon(Icons.arrow_forward_ios, size: 16),
               onTap: () {
